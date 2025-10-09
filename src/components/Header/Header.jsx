d1946cde3ec66b743087cbb40be4bb9531c29c0e
Viktor <vitygocanal@gmail.com>
2025-10-09 12:46:03 +0200

    add mobile header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,29 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
+import { IoMenu } from "react-icons/io5";
+import {useState} from 'react'
 
 
-export default function Header(){
-    return(
-        <header className={styles.header}>
-            <div className={styles.leftHeader}>
-                <div className={styles.logo}>
-                    FlashLogo
-                </div>
-                <ul className={styles.headerMenu}>
-                    <li className={styles.headerMenuElement}>data</li>
-                    <li className={styles.headerMenuElement}>data</li>
-                    <li className={styles.headerMenuElement}>data</li>
-                    <li className={styles.headerMenuElement}>data</li>
-                </ul>
-            </div>
-            <div className={styles.rightHeader}>
-                <div className={styles.buttons}>
-                    <button className={styles.headerButton}>login</button>
-                    <button className={styles.headerButton}>sign up</button>
-                    <button className={styles.hamburgerMenu}>|||</button>
-                </div>
-            </div>
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(true)
 
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <div className={styles.leftHeader}>
+        <div className={styles.logo}>FlashLogo</div>
+        <ul className={`${styles.headerMenu} ${menuOpen? styles.openMenu : ""}`}>
+          <li className={styles.headerMenuElement}>data</li>
+          <li className={styles.headerMenuElement}>data</li>
+          <li className={styles.headerMenuElement}>data</li>
+          <li className={styles.headerMenuElement}>data</li>
+        </ul>
+      </div>
+      <div className={styles.rightHeader}>
+        <div className={styles.buttons}>
+          <button className={styles.headerButton}>login</button>
+          <button className={`${styles.headerButton} ${styles.signUpButton}`}>sign up</button>
+        </div>
+        <button className={styles.hamburgerMenu} onClick={() => setMenuOpen(prev => !prev)}><IoMenu size="22px"/></button>
+      </div>
+    </header>
+  );
 }
